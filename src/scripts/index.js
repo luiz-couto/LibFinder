be90faff3d77ba26cac8317a71f7dcdb9ec4380f
luiz-couto <luiz.felipe.556@outlook.com>
2019-05-30 18:38:35 -0300

    Created the main function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -31,54 +31,59 @@
             container.appendChild(card)
 
             // Create an h1 and set the text content to the film's title
-            const h1 = document.createElement('h1')
-            card.appendChild(h1)
-            h1.textContent = lib.name
+            const a = document.createElement('a')
+            a.textContent = lib.name
+
+            a.addEventListener("click",event => {
+
+                event.preventDefault()
+        
+                var element = document.getElementById("lib_container")
+                while (element.firstChild) {
+                    element.removeChild(element.firstChild);
+                }
+
+                const url = 'https://api.cdnjs.com/libraries/' + lib.name
+        
+                fetch(url).then(response =>{
+                    return response.json();
+                }).then(lib_data =>{showLibData(lib_data)})
+        
+            })
             
+            card.appendChild(a)
 
         });
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// function go(){
-    
-//     fetch('https://api.cdnjs.com/libraries?search=npm').then(function(response) {
-//         if(response.ok) {
-//           response.blob().then(function(myBlob) {
-//             var objectURL = URL.createObjectURL(myBlob);
-//             console.log(objectURL)
-//           });
-//         } else {
-//           console.log('Network response was not ok.');
-//         }
-//       })
-//       .catch(function(error) {
-//         console.log('There has been a problem with your fetch operation: ' + error.message);
-//       });
-    
-//     return(
-//         alert("OK")
-//     )
-// }
-// async function fetchData() {
-
-//     URL = 'https://api.cdnjs.com/libraries?search=npm' + query
+    function showLibData(lib_data){
        
-//     const response = await fetch('https://api.cdnjs.com/libraries?search=npm');
-//     const json = await response.json();
-    
-//     alert(json);
-           
-   
-//   }
+        const container = document.getElementById("lib_container")
+       
+        const info = document.createElement('div')
+        info.setAttribute('class', 'info')
+        container.appendChild(info)
+
+        const name = document.createElement('h1')
+        name.textContent = lib_data.name
+        info.appendChild(name)
+
+        const description = document.createElement('h3')
+        description.textContent = lib_data.description
+        info.appendChild(description)
+
+        const homepage = document.createElement('p')
+        homepage.textContent = "Homepage: " + lib_data.homepage
+        info.appendChild(homepage)
+
+        const repository = document.createElement('p')
+        repository.textContent = "Repository: " + lib_data.repository.url
+        info.appendChild(repository)
+
+        const version = document.createElement('p')
+        version.textContent = "Version: " + lib_data.version
+        info.appendChild(version)
+
+    }
+
