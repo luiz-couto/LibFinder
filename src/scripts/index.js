9a39f9ad9378f13059d87e7f209affdaa914ddec
luiz-couto <luiz.felipe.556@outlook.com>
2019-05-31 12:27:39 -0300

    Styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,18 +23,25 @@
     function setData(data){
         
         const container = document.getElementById("lib_container")
+        var i = 1;
         
         data.results.forEach(lib => {
             
             const card = document.createElement('div')
-            card.setAttribute('class', 'card')
+            if(i%2 == 0){
+                card.setAttribute('class', 'card1')
+            }
+            else{
+                card.setAttribute('class', 'card2')
+            }
             container.appendChild(card)
 
             // Create an h1 and set the text content to the film's title
-            const a = document.createElement('a')
+            const a = document.createElement('p')
             a.textContent = lib.name
+            a.setAttribute('class','card_text')
 
-            a.addEventListener("click",event => {
+            card.addEventListener("click",event => {
 
                 event.preventDefault()
         
@@ -52,6 +59,7 @@
             })
             
             card.appendChild(a)
+            i++
 
         });
     }
@@ -67,22 +75,27 @@
 
         const name = document.createElement('h1')
         name.textContent = lib_data.name
+        name.setAttribute('id','name')
         info.appendChild(name)
 
         const description = document.createElement('h3')
         description.textContent = lib_data.description
+        description.setAttribute('id','description')
         info.appendChild(description)
 
         const homepage = document.createElement('p')
         homepage.textContent = "Homepage: " + lib_data.homepage
+        homepage.setAttribute('id','homepage')
         info.appendChild(homepage)
 
         const repository = document.createElement('p')
         repository.textContent = "Repository: " + lib_data.repository.url
+        repository.setAttribute('id','repository')
         info.appendChild(repository)
 
         const version = document.createElement('p')
         version.textContent = "Version: " + lib_data.version
+        version.setAttribute('id','version')
         info.appendChild(version)
 
     }
